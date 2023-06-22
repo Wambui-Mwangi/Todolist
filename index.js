33806fb877f93e6c9f54420963ae53d209595e81
Esther <mwangiestherwambuii@gmail.com>
2023-06-22 12:53:16 +0300

    updated POST

--- index.js
+++ index.js
@@ -1,55 +1,88 @@
-let tasksHolder = document.getElementById('fulltask');
+let tasksHolder = document.getElementById("fulltask");
+let addedTasks = document.getElementById("allTasks");
 
-const getTasks = async () => {
-  try {
-    const response = await fetch('https://dummyjson.com/todos');
-    if (!response.ok) {
-      throw new Error('failed to find tasks');
-    }
-    const taskData = await response.json();
-    return taskData.todos;
-  } catch (error) {
-    console.log('Error during fetching:', error);
-  }
-};
+fetch("https://dummyjson.com/todos?limit=10")
+  .then((response) => response.json())
+  .then((res) => {
+    console.log(res);
+    if (res.todos) {
+      res.todos.forEach((t) => {
+        let li = document.createElement("li");
+        let checkbox = document.createElement("input");
+        checkbox.checked = t.completed;
+        checkbox.type = "checkbox";
+        let label = document.createElement("p");
+        label.type = "text";
+        label.textContent = t.todo;
+        let deleteTasks = document.createElement("del");
 
-const displayTasks = async () => {
-  const tasks = await getTasks();
-  console.log(tasks);
-  if (Array.isArray(tasks)) {
-    tasks.forEach(item => {
-      let li = document.createElement('li');
-      let checkbox = document.createElement('input');
-      let label = document.createElement('label');
-      let deleteTask = document.createElement('del');
+        let editTask = document.createElement('button');
+        let deleteOneTask = document.createElement('button');
+        editTask.textContent = "Edit";
+        deleteOneTask.textContent = "Delete";
 
-      li.style.display = 'flex';
-      li.style.alignItems = 'center';
-
-      checkbox.type = 'checkbox';
-      checkbox.checked = item.completed;
-      label.textContent = item.todo;
-      deleteTask.textContent = 'Delete';
-      deleteTask.classList.add('deleteButton');
-
-      deleteTask.addEventListener('click', () => {
-        li.remove();
+        // editTask.addEventListener("click", (e) => {
+        //   e.preventDefault();
+        //   let editTask = document.createElement("input");
+        //   editTask.type = "text";
+        //   editTask.value = t.todo;
+        //   li.replaceChild(editTask, label);
+        li.style.display = "flex";
+        li.style.alignItems = "center";
+        li.style.gap = "2px";
+        addedTasks.appendChild(li);
+        li.appendChild(checkbox);
+        li.appendChild(label);
+        li.appendChild(deleteTasks);
+        li.appendChild(editTask)
+        li.appendChild(deleteOneTask)
       });
-
-      li.appendChild(checkbox);
-      li.appendChild(label);
-      li.appendChild(deleteTask);
-      tasksHolder.appendChild(li);
-    });
-  }
-};
-
-const clearTasks = () => {
-  const done = tasksHolder.querySelectorAll('li');
-  done.forEach(task => {
-    const checks = task.querySelector('input[type="checkbox"]');
-    if (checks.checked) {
-      task.remove();
+    } else {
+      console.log("Tasks not found", error);
     }
+  })
+  .catch((error) => {
+    console.log("Tasks not located", error);
   });
-};
+
+let addButton = document.getElementById("addButton");
+let deleteTasks = document.getElementById("clear");
+let newaddedTask = document.getElementById("newTask");
+
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  fetch("https://dummyjson.com/todos/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      todo: newaddedTask.value,
+      completed: false,
+      userId: 23,
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      let li = document.createElement("li");
+      let checkbox = document.createElement("input");
+      const newT = document.createElement("p");
+      li.style.display = "flex";
+      li.style.alignItems = "center";
+      li.style.gap = "2px";
+      checkbox.type = "checkbox";
+      let editTask = document.createElement('button');
+      let deleteOneTask = document.createElement('button');
+      editTask.textContent = "Edit";
+      deleteOneTask.textContent = "Delete";
+      li.appendChild(checkbox);
+      li.appendChild(newT);
+      li.appendChild(editTask)
+      li.appendChild(deleteOneTask)
+      addedTasks.appendChild(li);
+      newT.textContent = response.todo;
+    })
+    .catch((error) => error);
+});

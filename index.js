21a128c23688fd229c75153b41dbb1dfb6ae4d3d
Esther <mwangiestherwambuii@gmail.com>
2023-06-22 21:27:23 +0300

    CRUD

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ fetch("https://dummyjson.com/todos?limit=10")
   .then((response) => response.json())
   .then((res) => {
     console.log(res);
+
     if (res.todos) {
       res.todos.forEach((t) => {
         let li = document.createElement("li");
@@ -15,18 +16,11 @@ fetch("https://dummyjson.com/todos?limit=10")
         label.type = "text";
         label.textContent = t.todo;
         let deleteTasks = document.createElement("del");
-
-        let editTask = document.createElement('button');
-        let deleteOneTask = document.createElement('button');
+        let editTask = document.createElement("button");
+        let deleteOneTask = document.createElement("button");
         editTask.textContent = "Edit";
         deleteOneTask.textContent = "Delete";
 
-        // editTask.addEventListener("click", (e) => {
-        //   e.preventDefault();
-        //   let editTask = document.createElement("input");
-        //   editTask.type = "text";
-        //   editTask.value = t.todo;
-        //   li.replaceChild(editTask, label);
         li.style.display = "flex";
         li.style.alignItems = "center";
         li.style.gap = "2px";
@@ -34,23 +28,27 @@ fetch("https://dummyjson.com/todos?limit=10")
         li.appendChild(checkbox);
         li.appendChild(label);
         li.appendChild(deleteTasks);
-        li.appendChild(editTask)
-        li.appendChild(deleteOneTask)
+        li.appendChild(editTask);
+        li.appendChild(deleteOneTask);
       });
     } else {
-      console.log("Tasks not found", error);
+      console.log("Tasks not found");
     }
   })
-  .catch((error) => {
-    console.log("Tasks not located", error);
-  });
+  .catch((error) => error);
 
 let addButton = document.getElementById("addButton");
 let deleteTasks = document.getElementById("clear");
 let newaddedTask = document.getElementById("newTask");
+let editTask = document.createElement("button");
+let deleteOneTask = document.createElement("button");
+editTask.textContent = "Edit";
+deleteOneTask.textContent = "Delete";
 
-addButton.addEventListener("click", (e) => {
-  e.preventDefault();
+
+// post
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
 
   fetch("https://dummyjson.com/todos/add", {
     method: "POST",
@@ -66,6 +64,7 @@ addButton.addEventListener("click", (e) => {
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
+
       let li = document.createElement("li");
       let checkbox = document.createElement("input");
       const newT = document.createElement("p");
@@ -73,16 +72,55 @@ addButton.addEventListener("click", (e) => {
       li.style.alignItems = "center";
       li.style.gap = "2px";
       checkbox.type = "checkbox";
-      let editTask = document.createElement('button');
-      let deleteOneTask = document.createElement('button');
       editTask.textContent = "Edit";
       deleteOneTask.textContent = "Delete";
+
       li.appendChild(checkbox);
       li.appendChild(newT);
-      li.appendChild(editTask)
-      li.appendChild(deleteOneTask)
+      li.appendChild(editTask);
+      li.appendChild(deleteOneTask);
       addedTasks.appendChild(li);
       newT.textContent = response.todo;
     })
     .catch((error) => error);
 });
+
+
+// put
+editTask.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  fetch("https://dummyjson.com/todos/23", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      todo: newaddedTask.value,
+      completed: true,
+      userId: 23,
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error)=>error)
+});
+
+
+// delete
+deleteOneTask.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  fetch("https://dummyjson.com/todos/23", {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      newaddedTask.remove()
+    })
+    .catch((error) => error)
+});
+
